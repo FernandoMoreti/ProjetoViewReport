afbd1464b593c704c715eb3d02c7500c29e917b6
FernandoMoreti <fernandombolela@gmail.com>
2026-08-18 14:01:01 -0300

    UPDATE apis url

--- my-app/app/extraction/page.tsx
+++ my-app/app/extraction/page.tsx
@@ -44,7 +44,7 @@ function App() {
 
     try {
 
-      const responseFile = await fetch('http://localhost:3008/api/rpa/', {
+      const responseFile = await fetch('http://192.168.1.90:3008/api/rpa/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ bank: banco }),
@@ -84,7 +84,7 @@ function App() {
               formData.append('banco', banco)
               formData.append('arquivo', fileData)
 
-              const response = await fetch("http://127.0.0.1:5000/execute", {
+              const response = await fetch("http://192.168.1.90:5000/execute", {
                 method: "POST",
                 body: formData,
               })
@@ -100,7 +100,7 @@ function App() {
 
               const listOfProposal = responseData.listOfProposal;
               try {
-                const responseProposals = await axios.post("http://localhost:3003/proposal", listOfProposal)
+                const responseProposals = await axios.post("http://192.168.1.90:30000/proposal", listOfProposal)
 
                 if (!responseProposals.status) throw new Error("Erro ao salvar propostas no banco de dados")
 
@@ -135,7 +135,7 @@ function App() {
                 processedAt: null
               }
 
-              await axios.post("http://localhost:3003/reports", { bank, reports: [report] });
+              await axios.post("http://192.168.1.90:30000/reports", { bank, reports: [report] });
 
               alert("Dados salvos com sucesso!");
               setValidar(true);
@@ -171,60 +171,60 @@ function App() {
   ]
 
   return (
-      <div className='flex flex-col h-full'>
-        <section className='flex flex-col h-full items-center bg-[#1a0b2e] text-gray-100 font-sans'>
-          <p className={`absolute z-10 p-3 text-white transition-opacity duration-500 rounded-b-2xl font-bold text-center ${mostrar ? ' opacity-100 ' : ' opacity-0 '}${validar ? 'bg-purple-600 shadow-[0_0_15px_rgba(168,85,247,0.4)]' : 'bg-red-600'}`}>
-            {validar ? "Editado com sucesso" : mensagem ? "Faltando credenciais" : errorMessage}
-          </p>
+    <div className='flex flex-col h-full'>
+      <section className='flex flex-col h-full items-center bg-[#1a0b2e] text-gray-100 font-sans'>
+        <p className={`absolute z-10 p-3 text-white transition-opacity duration-500 rounded-b-2xl font-bold text-center ${mostrar ? ' opacity-100 ' : ' opacity-0 '}${validar ? 'bg-purple-600 shadow-[0_0_15px_rgba(168,85,247,0.4)]' : 'bg-red-600'}`}>
+          {validar ? "Editado com sucesso" : mensagem ? "Faltando credenciais" : errorMessage}
+        </p>
 
-          <div className='flex-1 flex gap-10 p-20 justify-center items-center backdrop-blur-sm'>
-            <div className='flex flex-col justify-center items-center p-10 gap-20 h-full w-150 bg-[#1e132f] border border-purple-500/20 shadow-2xl shadow-black/50 rounded-4xl'>
-              <header className='text-center'>
-                <h1 className='text-2xl font-bold bg-linear-to-r from-purple-300 to-purple-600 bg-clip-text text-transparent'>
-                  Extração de Relatórios
-                </h1>
-              </header>
+        <div className='flex-1 flex gap-10 p-20 justify-center items-center backdrop-blur-sm'>
+          <div className='flex flex-col justify-center items-center p-10 gap-20 h-full w-150 bg-[#1e132f] border border-purple-500/20 shadow-2xl shadow-black/50 rounded-4xl'>
+            <header className='text-center'>
+              <h1 className='text-2xl font-bold bg-linear-to-r from-purple-300 to-purple-600 bg-clip-text text-transparent'>
+                Extração de Relatórios
+              </h1>
+            </header>
 
-              <form onSubmit={handleSubmit} className='flex flex-col justify-center gap-5 w-full' action="">
+            <form onSubmit={handleSubmit} className='flex flex-col justify-center gap-5 w-full' action="">
 
-                <div>
-                  <p className='text-sm font-bold text-gray-400 uppercase tracking-tighter mb-2'>Escolha o banco:</p>
-                  <select
-                    onChange={(e) => {setBanco(e.target.value)}}
-                    className='w-full bg-[#0f081a] border border-gray-800 mt-1 rounded-xl p-4 text-gray-200 shadow-inner cursor-pointer focus:border-purple-500 outline-none appearance-none transition-all' 
-                    required
-                    name="Banco"
-                    id=""
-                  >
-                    <option className='hidden'>Escolha um banco</option>
-                      {bancos.map((banco) => (
-                        <option key={banco} value={banco} className="bg-[#1e132f]">{banco}</option>
-                      ))}
-                  </select>
-                </div>
-
-                <button
-                    className='bg-purple-600 text-white font-bold rounded-xl p-5 transition-all duration-300 cursor-pointer hover:bg-purple-500 hover:-translate-y-1 shadow-[0_0_20px_rgba(168,85,247,0.3)] active:scale-95' 
-                    type='submit'
+              <div>
+                <p className='text-sm font-bold text-gray-400 uppercase tracking-tighter mb-2'>Escolha o banco:</p>
+                <select
+                  onChange={(e) => {setBanco(e.target.value)}}
+                  className='w-full bg-[#0f081a] border border-gray-800 mt-1 rounded-xl p-4 text-gray-200 shadow-inner cursor-pointer focus:border-purple-500 outline-none appearance-none transition-all' 
+                  required
+                  name="Banco"
+                  id=""
                 >
-                  {loading ? (
-                    <span className="flex items-center justify-center gap-2">
-                      <div className="w-4 h-4 border-2 border-white/30 border-t-white rounded-full animate-spin"></div>
-                      Carregando...
-                    </span>
-                  ) : "Extrair arquivo"}
-                </button>
-              </form>
-            </div>
-            <div className='flex flex-col h-full w-full bg-[#1e132f] border border-purple-500/20 shadow-xl rounded-2xl p-5'>
-              <p className='text-sm font-bold text-gray-400 uppercase tracking-tighter mb-2'>Console de Execução:</p>
-              <div className='bg-[#0f081a] text-green-400 font-mono p-4 rounded-xl h-full overflow-y-auto'>
-                {logs.map((log, index) => <div key={index}>{log}</div>)}
+                  <option className='hidden'>Escolha um banco</option>
+                    {bancos.map((banco) => (
+                      <option key={banco} value={banco} className="bg-[#1e132f]">{banco}</option>
+                    ))}
+                </select>
               </div>
+
+              <button
+                  className='bg-purple-600 text-white font-bold rounded-xl p-5 transition-all duration-300 cursor-pointer hover:bg-purple-500 hover:-translate-y-1 shadow-[0_0_20px_rgba(168,85,247,0.3)] active:scale-95' 
+                  type='submit'
+              >
+                {loading ? (
+                  <span className="flex items-center justify-center gap-2">
+                    <div className="w-4 h-4 border-2 border-white/30 border-t-white rounded-full animate-spin"></div>
+                    Carregando...
+                  </span>
+                ) : "Extrair arquivo"}
+              </button>
+            </form>
+          </div>
+          <div className='flex flex-col h-full w-full bg-[#1e132f] border border-purple-500/20 shadow-xl rounded-2xl p-5'>
+            <p className='text-sm font-bold text-gray-400 uppercase tracking-tighter mb-2'>Console de Execução:</p>
+            <div className='bg-[#0f081a] text-green-400 font-mono p-4 rounded-xl h-full overflow-y-auto'>
+              {logs.map((log, index) => <div key={index}>{log}</div>)}
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
